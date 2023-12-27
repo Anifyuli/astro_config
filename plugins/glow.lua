3e512a368fec8ae28fc5db415a1c621b96bccb4d
Moh Anif Yuliansyah <anifyuliansyah@gmail.com>
2023-12-27 21:50:32 +0700

    Update glow.lua

--- plugins/glow.lua
+++ plugins/glow.lua
@@ -1,13 +1,5 @@
 return {
   "ellisonleao/glow.nvim",
-  enable = true,
-  lazy = false,
-  pin = true,
   config = true,
   cmd = "Glow",
-  opts = {
-    require('glow').setup({
-      border = "rounded"
-    })
-  }
 }
