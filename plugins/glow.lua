3141c259f63649b09bf931dc3aaa66732ef6dee8
Anifyuli <anifyuliansyah@gmail.com>
2023-12-27 21:48:26 +0700

    Updating Glow

--- plugins/glow.lua
+++ plugins/glow.lua
@@ -1,14 +1,5 @@
 return {
   "ellisonleao/glow.nvim",
-  enabled = true,
-  priority = 1000,
-  lazy = false,
-  pin = true,
   config = true,
   cmd = "Glow",
-  opts = {
-    require('glow').setup({
-      border = "rounded"
-    })
-  }
 }
